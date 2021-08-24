52f43e81b8f7af9f38d7afd537ba4b800ec03dd6
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-08-24 01:17:43 -0500

    imports all project photos dynamically

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react'
-const data = require('../assets/json/projects.json');
+import React, { useState } from 'react';
+var data = require('../assets/json/projects.json');
 
+function importAll(r) {
+    return r.keys().map(r);
+}
+
+const images = importAll(require.context('../assets/projects/', false, /\.(png|jpe?g|svg)$/));
 
 function Projects() {
-    console.log('data',data)
+    console.log('images', images)
 
     function createProjectCards() {
         for (var i = 0; i < data.length; i++) {
             var obj = data[i];
-            console.log(`Name: ${obj}`);
+            console.log(`Name: ${obj} ${'../assets/projects/' + obj.name + '.png'}`);
+            return (
+                <div class="project">
+                    <h1>{obj.name}</h1>
+                    <p>{obj.description}</p>
+                    <img src={images[0].default} alt="project demo screenshot"></img>
+                    <a href={obj.deployment} target="_blank" rel="noreferrer">Deployment</a>
+                    <a href={obj.repository} target="_blank" rel="noreferrer">Repository</a>
+                </div>
+            );
         }
     }
 
